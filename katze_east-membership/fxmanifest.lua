000ba2108ccf0a4eb3e7481fb606cd9676b8d304
FrankfurtRPDeveloper <132407218+FrankfurtRPDeveloper@users.noreply.github.com>
2023-05-07 21:50:33 +0200

    Add files via upload

---
+++ katze_east-membership/fxmanifest.lua
@@ -0,0 +1,18 @@
+resource_manifest_version "cerulan"
+game "gta5"
+
+client_script "client/cl.lua"
+
+ui_page "html/index.html"
+
+description "Eine UI Für eine Mitgliedschaft"
+author "Katze"
+
+files {
+   "html/index.html",
+   "html/index.js",
+   "html/index.css",
+   "html/reset.css",
+   "client/cl.lua"
+   "nui/nui.lua"
+}
---
+++ katze_east-membership/nui/nui.lua
@@ -0,0 +1,53 @@
+-- Copyright Katze Scripts 2023
+
+local display = false
+
+RegisterCommand("eastmenutest", function(source)
+    setDisplay(not display)
+end)
+
+RegisterNUICallback("main", function(data)
+    chat(data.text, {0,255,0})
+    setDisplay(false)
+
+end)
+
+RegisterNUICallback("error", function(data)
+    chat(data.error, {255,0,0})
+    SetDisplay(false)
+end)
+
+RegisterNUICallback("exit", function(data)
+    chat("Exited", {0,255,0})
+    setDisplay(false)
+end)
+
+Citizen.CreateThread(function()
+    while display do
+        Citizen.Wait(0)
+    
+    DisableControlAction(0, 1, display)
+    DisableControlAction(0, 2, display)
+    DisableControlAction(0, 142, display)
+    DisableControlAction(0, 18, display)
+    DisableControlAction(0, 322, display)
+    DisableControlAction(0, 106, display)
+    end
+end)
+
+function setDisplay(bool)
+    display = bool
+    SetNuiFocus(bool, bool)
+    SendNUIMessage({
+        type="ui",
+        status = bool,
+    })
+end
+
+function chat(str, color)
+    TriggerEvent("chat:addMessage", {
+        color = color,
+        multiline= true,
+        args = {str}
+)}
+end
